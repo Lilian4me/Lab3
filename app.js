5ac663b1f2d1869587173b9d76f1638c655b8462
lilian.isibor@yahoo.com <lisibor@DESKTOP-DCCAF4G>
2023-09-21 23:48:27 +0100

    commit

---
+++ app.js
@@ -0,0 +1,35 @@
+// for (initialisation; condition; increment)
+// initialisation happens ONCE before the loop starts
+// condition is checked before each loop happens (and the loop doesn't if the condition is false)
+// increment happens AFTER every loop (so the condition isn't checking the same number every time)
+
+let answer = 7;
+let number = prompt("Guess a number between 1-10");
+let attempts = 4;
+
+while (attempts > 1) {
+  attempts--;
+  if (number > answer) {
+    alert("Too high!");
+    number = prompt("Guess again! You have " + attempts + " left");
+  } else if (number < answer) {
+    alert("Too low!");
+    number = prompt("Guess again! You have " + attempts + " left");
+  } else if (parseInt(number) === answer) {
+    alert("That's right, lucky number 7!");
+    break;
+  }
+}
+
+if (parseInt(number) !== answer) {
+  alert("The answer was 7");
+}
+
+// start of questions 7
+let plants = ["Rose", "Lily", "Sunflower", "Barbados"];
+
+let plantsGuess = prompt("What is one of my favourite plants?");
+
+if (plants.indexOf(plantsGuess) !== -1) {
+  alert("Thats correct! I really like " + plantsGuess);
+}
